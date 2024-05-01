30aabc67dcccdefb8bac10fee62b088576cc835a
dawngn <haidangdavid123@gmail.com>
2024-05-01 19:36:41 +0700

    clean up

--- dist/middleware/routeNotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFound = void 0;
-function routeNotFound(req, res, next) {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    logging.error(`[404] - ${req.originalUrl} - Not Found`);
-    return res.status(404).json({ error: error.message });
-}
-exports.routeNotFound = routeNotFound;
